80d1810a5cbadbfe0f9fcf6ac87538bd507cebc4
Ahmed Alnaimi <164284383+Fv3g@users.noreply.github.com>
2026-08-16 01:53:05 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import App from './routes/index'
 import './index.css'
-
-const router = createRouter({ routeTree })
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
