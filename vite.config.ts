66633fb5e464f0a64eb9aacb005c8be8e3d7dc8d
Ahmed Alnaimi <164284383+Fv3g@users.noreply.github.com>
2026-08-16 11:51:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tanstackStart(),
     react(),
     tsconfigPaths(),
   ],
