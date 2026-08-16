507155a9f35278570d3604ec2b5e517005bdff02
Ahmed Alnaimi <164284383+Fv3g@users.noreply.github.com>
2026-08-16 11:10:32 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './routes/index'
+import HiraStore from './routes/index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <HiraStore />
   </React.StrictMode>,
 )
